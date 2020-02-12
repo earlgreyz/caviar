63c1f213d221603af50f49fa6a261384a83b4824
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2020-02-12 13:42:12 +0100

    Car dispatcher for NL-model cars

---
+++ src/simulator/dispatcher/car.py
@@ -0,0 +1,29 @@
+import random
+
+from simulator.road.road import Road
+from simulator.vehicle.car import Car
+
+
+class CarDispatcher:
+    count: int
+    remaining: int
+    road: Road
+
+    def __init__(self, count: int, road: Road):
+        self.count = count
+        self.remaining = 0
+        self.road = road
+
+    def dispatch(self) -> None:
+        self.remaining += random.randint(0, self.count)
+        lanes = list(range(self.road.lanes_count))
+        random.shuffle(lanes)
+        for lane in lanes:
+            if self.remaining <= 0:
+                return
+            if 0 in self.road.lanes[lane]:
+                return
+            position = (0, lane)
+            car = Car(position=position, velocity=self.road.params.MAX_SPEED, road=self.road)
+            self.road.addVehicle(position=position, vehicle=car)
+            self.remaining -= 1
