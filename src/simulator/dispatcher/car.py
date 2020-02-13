d07e1c47777e7e2673457c6cdea7c8df3fa48c10
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2020-02-13 19:42:32 +0100

    Fix car dispatcher stopping on occupied lane encounter

--- src/simulator/dispatcher/car.py
+++ src/simulator/dispatcher/car.py
@@ -22,9 +22,11 @@ class CarDispatcher:
         for lane in lanes:
             if self.remaining <= 0:
                 return
-            if 0 in self.road.lanes[lane]:
-                return
+            # Check if position is not occupied.
             position = (0, lane)
+            if self.road.getVehicle(position=position) is not None:
+                continue
+            # Place the car.
             car = Car(position, self.road.params.MAX_SPEED, road=self.road, params=self.params)
             self.road.addVehicle(position=position, vehicle=car)
             self.remaining -= 1
