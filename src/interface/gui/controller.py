dd17b8ec965a3db7c6a6f87e7723c95f777a60d7
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2020-02-19 14:56:38 +0100

    Show obstacles on the visualisation

--- src/interface/gui/controller.py
+++ src/interface/gui/controller.py
@@ -1,9 +1,12 @@
 import pygame
 
 from simulator.simulator import Simulator
+from simulator.vehicle.obstacle import Obstacle
+from simulator.vehicle.vehicle import Vehicle
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+CL_OBSTACLE = (67, 67, 78)
+CL_ROAD = (212, 212, 212)
+CL_VEHICLE = (27, 176, 66)
 
 
 class Controller:
@@ -19,11 +22,22 @@ class Controller:
 
     def drawVehicles(self, factor: float):
         for vehicle in self.simulator.road.getAllVehicles():
-            ax, ay = vehicle.last_position
-            bx, by = vehicle.position
-            x, y = ax + (bx - ax) * factor, ay + (by - ay) * factor
-            rect = (x * self.SIZE, y * self.SIZE, self.SIZE, self.SIZE)
-            pygame.draw.rect(self.screen, BLACK, rect)
+            if isinstance(vehicle, Obstacle):
+                self.drawObstacle(vehicle)
+            else:
+                self.drawVehicle(vehicle, factor)
+
+    def drawObstacle(self, obstacle: Obstacle):
+        x, y = obstacle.position
+        rect = (x * self.SIZE, y * self.SIZE, self.SIZE, self.SIZE)
+        pygame.draw.rect(self.screen, CL_OBSTACLE, rect)
+
+    def drawVehicle(self, vehicle: Vehicle, factor: float):
+        ax, ay = vehicle.last_position
+        bx, by = vehicle.position
+        x, y = ax + (bx - ax) * factor, ay + (by - ay) * factor
+        rect = (x * self.SIZE, y * self.SIZE, self.SIZE, self.SIZE)
+        pygame.draw.ellipse(self.screen, CL_VEHICLE, rect)
 
     def run(self, speed: float = 100., refresh: int = 60):
         clock = pygame.time.Clock()
@@ -46,7 +60,7 @@ class Controller:
         pygame.quit()
 
     def draw(self, passed: float, speed: float):
-        self.screen.fill(WHITE)
+        self.screen.fill(CL_ROAD)
         self.drawVehicles(passed / speed)
         pygame.display.flip()
         pygame.display.update()
