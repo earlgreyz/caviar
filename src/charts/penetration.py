61bf910ab2455c25ab000e7d705809309fea2e82
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2020-10-17 04:23:02 +0100

    Print table

--- src/charts/penetration.py
+++ src/charts/penetration.py
@@ -8,7 +8,7 @@ import typing
 
 
 def make_plot(df: pd.DataFrame, key: str, ylabel: str, multiplier: int, ylim: int,
-              output, prefix) -> None:
+              table: bool, output, prefix) -> None:
     # Prepare data.
     key_all = f'{key}_all'
     data_all = df[['x', key_all]] \
@@ -24,6 +24,17 @@ def make_plot(df: pd.DataFrame, key: str, ylabel: str, multiplier: int, ylim: in
     data_autonomous['type'] = 'Autonomous'
     data = data_all.append(data_conventional).append(data_autonomous)
     data['y'] *= multiplier
+
+    if table:
+        print(key)
+        df[key_all] *= multiplier
+        df[key_autonomous] *= multiplier
+        df[key_conventional] *= multiplier
+        print(df.groupby(['x'], as_index=False).agg({
+            f'{key}_all': ['mean', 'std'],
+            f'{key}_conventional': ['mean', 'std'],
+            f'{key}_autonomous': ['mean', 'std'],
+        }).to_csv(index=False, float_format='%.3f'))
     # Make plot.
     sns.set_style('white')
     f = plt.figure(figsize=(6, 4))
@@ -45,8 +56,9 @@ def make_plot(df: pd.DataFrame, key: str, ylabel: str, multiplier: int, ylim: in
 @click.command()
 @click.option('--output', '-o', default=None, help='Save output to a directory')
 @click.option('--prefix', '-p', default='', help='Prefix for output file names')
+@click.option('--table', '-t', is_flag=True, help='Print the output table for the graph')
 @click.argument('file', type=click.File())
-def main(output: typing.Optional[str], prefix: str, file):
+def main(output: typing.Optional[str], prefix: str, table: bool, file):
     df = pd.read_csv(file, header=0)
     keys = ['velocity', 'throughput', 'decelerations', 'laneChanges', 'waiting']
     ylabels = [
@@ -59,7 +71,7 @@ def main(output: typing.Optional[str], prefix: str, file):
     multipliers = [1, 1, 100, 100, 100]
     ylims = [5.5, 3.5, 6.5, 3, 60]
     for key, ylabel, multiplier, ylim in zip(keys, ylabels, multipliers, ylims):
-        make_plot(df, key, ylabel, multiplier, ylim, output, prefix)
+        make_plot(df, key, ylabel, multiplier, ylim, table, output, prefix)
 
 
 if __name__ == '__main__':
